9b992f455611e54f3f07c5017b162db5858c93ea
Miguel Pimentel <miguelpimentel2013@gmail.com>
2019-07-05 04:57:26 -0300

    Minimal changes

--- file.go
+++ file.go
@@ -1,15 +1,15 @@
-package main
+// package main
 
-import (
-    "fmt"
-	"strings"
-)
+// import (
+//     // "fmt"
+// 	// "strings"
+// )
 
-func formatSensorData(string content) {
-	sensorsData := [10][100000]int
-	numberOfRows := strings.Count(content, "\n")
+// func formatSensorData(string content) {
+// 	// sensorsData := [10][100000]int
+// 	// numberOfRows := strings.Count(content, "\n")
 
-	fmt.Printf("[%v] string has %d of characters of [a] ", strings., numberOfRows)
+// 	// // fmt.Printf("[%v] string has %d of characters of [a] ", strings., numberOfRows)
 
 
-}
+// }
